f0916ea183e593363f5ce0b1779c8703c6a20964
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-11-04 12:39:36 +0200

    .

--- frontend-user/src/App.jsx
+++ frontend-user/src/App.jsx
@@ -4,12 +4,21 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/details/:id" element={<BookDetails />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route path="/details/:id" element={<BookDetails />} />
+    //   </Routes>
+    // </BrowserRouter>
+
+    <div className="bg-gray-100 min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-blue-600 mb-4">
+        Hello Tailwind CSS!
+      </h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Tailwind Button
+      </button>
+    </div>
   );
 }
 
