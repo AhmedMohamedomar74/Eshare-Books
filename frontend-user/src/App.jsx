0ccceb7708b6ba8190a4dbefe9f0c10668d56042
ShenoudaDaniel07 <shenoudadaniel777@gmail.com>
2025-11-05 21:48:59 +0200

    .

--- frontend-user/src/App.jsx
+++ frontend-user/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BookDetails from "./pages/Book-Details/BookDetails";
-import Navbar from "./components/Navbar";
-import BookCycleLogin from "./pages/login/Login.jsx";
-import BookShareRegister from "./pages/register/Register.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BookDetails from './pages/Book-Details/BookDetails';
+import Navbar from './components/Navbar';
+import BookCycleLogin from './pages/login/Login.jsx';
+import BookShareRegister from './pages/register/Register.jsx';
+import MyReports from './pages/MyReports/MyReports.jsx';
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/details/:id" element={<BookDetails />} />
-        <Route path="/login" element={<BookCycleLogin/>} />
-        <Route path="/register" element={<BookShareRegister/>} />
+        <Route path="/login" element={<BookCycleLogin />} />
+        <Route path="/register" element={<BookShareRegister />} />
+        <Route path="/reports" element={<MyReports />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend-user/src/components/ReportsComponents/MyReportsTable.jsx
@@ -0,0 +1,33 @@
+import { Table, TableBody, TableContainer, Paper } from '@mui/material';
+import MyReportsTableHead from './MyReportsTableHead';
+import MyReportsTableRow from './MyReportsTableRow';
+import MyReportsEmptyState from './MyReportsEmptyState';
+
+export default function MyReportsTable({ reportsData, getStatusColor, getStatusTextColor }) {
+  const hasReports = reportsData.length > 0;
+
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{ backgroundColor: '#fff', boxShadow: '0 1px 3px rgba(0,0,0,0.1)' }}
+    >
+      {hasReports ? (
+        <Table>
+          <MyReportsTableHead />
+          <TableBody>
+            {reportsData.map((row) => (
+              <MyReportsTableRow
+                key={row.id}
+                row={row}
+                getStatusColor={getStatusColor}
+                getStatusTextColor={getStatusTextColor}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <MyReportsEmptyState />
+      )}
+    </TableContainer>
+  );
+}
