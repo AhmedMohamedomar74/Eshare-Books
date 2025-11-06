80a4f278edbbbd9fbb812216bb2ef57a1c929c3c
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-11-06 13:08:17 +0200

    feat: add BookShareDashboard component and integrate into routing

--- frontend-user/src/App.jsx
+++ frontend-user/src/App.jsx
@@ -3,6 +3,7 @@ import BookDetails from "./pages/Book-Details/BookDetails";
 import Navbar from "./components/Navbar";
 import BookCycleLogin from "./pages/login/Login.jsx";
 import BookShareRegister from "./pages/register/Register.jsx";
+import BookShareDashboard from "./pages/profile/Profile.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/details/:id" element={<BookDetails />} />
         <Route path="/login" element={<BookCycleLogin/>} />
         <Route path="/register" element={<BookShareRegister/>} />
+        <Route path="/profile" element={<BookShareDashboard/>} />
       </Routes>
     </BrowserRouter>
   );
